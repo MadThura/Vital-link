b150bbf7d0729e04e8ad6a2948e3864f62a0cbd4
v-io-lin <thetmgmg.123875@gmail.com>
2025-07-23 21:22:03 +0630

    test1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tailwind with Laravel</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-gray-100">
+    <h1 class="text-3xl font-bold underline text-center mt-10">
+        Hello world!
+    </h1>
+</body>
+</html>
+
