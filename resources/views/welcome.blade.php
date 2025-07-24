fced423af286f0357f07076d8b38dee82835fa6c
v-io-lin <thetmgmg.123875@gmail.com>
2025-07-24 09:58:37 +0630

    adding_photos_and_sign_up_form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tailwind with Laravel</title>
+    <title>Vital Link</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
