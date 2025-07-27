9f5eca8f14d1e4d6ee6661906ade9bec14bce24b
v-io-lin <thetmgmg.123875@gmail.com>
2025-07-27 14:13:13 +0630

    alert-box

--- resources/views/admin/donors/donor-show.blade.php
+++ resources/views/admin/donors/donor-show.blade.php
@@ -1,5 +1,36 @@
 <x-admin-layout>
-    <x-search-bar/>
+    <x-search-bar />
+    {{-- resources/views/components/status-alert.blade.php --}}
+    @if (session()->has('status'))
+        @php
+            $status = session('status');
+            $messages = [
+                'approved' => ['text' => 'Donor approved successfully!', 'color' => 'green'],
+                'rejected' => ['text' => 'Donor rejected.', 'color' => 'rose'],
+                'suspended' => ['text' => 'Donor suspended.', 'color' => 'yellow'],
+                'deleted' => ['text' => 'Donor deleted.', 'color' => 'gray'],
+                'edited' => ['text' => 'Donor information updated.', 'color' => 'blue'],
+            ];
+        @endphp
+
+        @if (isset($messages[$status]))
+            <div id="alertBox"
+                class="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 w-full max-w-md shadow-xl rounded-xl overflow-hidden">
+                <div
+                    class="bg-{{ $messages[$status]['color'] }}-100 text-{{ $messages[$status]['color'] }}-800
+                        flex items-start justify-between gap-4 px-6 py-4">
+                    <span class="text-base font-medium">
+                        {{ $messages[$status]['text'] }}
+                    </span>
+                    <button onclick="document.getElementById('alertBox').style.display='none'"
+                        class="text-gray-600 hover:text-black transition cursor-pointer">
+                        &times;
+                    </button>
+                </div>
+            </div>
+        @endif
+    @endif
+
     <!-- Donor Table Area (scrollable) -->
     <div class="flex-grow overflow-y-auto p-5 bg-gray-800 [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
         <h3 class="text-lg mb-4 font-medium text-gray-200 flex items-center gap-2">
@@ -43,11 +74,11 @@
                                 <x-tooltip-button peerClass="view" tooltipText="View" icon="fa-eye"
                                     hoverColor="cyan-400" route="/" />
                                 <x-tooltip-button peerClass="edit" tooltipText="Edit" icon="fa-pen-to-square"
-                                    hoverColor="emerald-400" route="/"/>
+                                    hoverColor="emerald-400" route="/" />
                                 <x-tooltip-button peerClass="delete" tooltipText="Delete" icon="fa-trash"
-                                    hoverColor="rose-500" route="/"/>
-                                <x-tooltip-button peerClass="ban" tooltipText="Ban" icon="fa-ban"
-                                    hoverColor="rose-500" route="/"/>
+                                    hoverColor="rose-500" route="/" />
+                                <x-tooltip-button peerClass="ban" tooltipText="Ban" icon="fa-ban" hoverColor="rose-500"
+                                    route="/" />
                             </div>
                         </td>
 
@@ -74,11 +105,11 @@
                         <td class="py-3 px-3 text-center">
                             <div class="flex justify-center gap-1">
                                 <x-tooltip-button peerClass="view" tooltipText="View" icon="fa-eye"
-                                    hoverColor="cyan-400" route="/"/>
+                                    hoverColor="cyan-400" route="/" />
                                 <x-tooltip-button peerClass="approve" tooltipText="Approve" icon="fa-check"
-                                    hoverColor="emerald-400" route="/"/>
+                                    hoverColor="emerald-400" route="/donor-show/approved" />
                                 <x-tooltip-button peerClass="reject" tooltipText="Approve" icon="fa-xmark"
-                                    hoverColor="rose-500" route="/"/>
+                                    hoverColor="rose-500" route="/" />
                             </div>
                         </td>
                     </tr>
