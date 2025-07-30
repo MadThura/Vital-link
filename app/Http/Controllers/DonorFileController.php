0b1dfbcdf66c8296debb8c1fdddcf9e6ee143b7e
MadThura <thura36094@gmail.com>
2025-07-31 00:38:40 +0630

    Refactor code structure for improved readability and maintainability

--- app/Http/Controllers/DonorFileController.php
+++ app/Http/Controllers/DonorFileController.php
@@ -13,21 +13,13 @@ class DonorFileController extends Controller
     {
         $user = Auth::user();
 
-<<<<<<< HEAD
-        $donor = Donor::where('nrc_front', 'like', "%$path%")
-=======
         $donor = Donor::where('profile_img', 'like', "%$path%")
             ->orwhere('nrc_front', 'like', "%$path%")
->>>>>>> fb6e7b4d91676f400e0d4390e9af14f83f4ef560
             ->orWhere('nrc_back', 'like', "%$path%")
             ->orWhere('health_certificate', 'like', "%$path%")
             ->firstOrFail();
 
-<<<<<<< HEAD
-        if ($user->role !== 'blood_bank_admin' || $donor->user_id !== $user->id) {
-=======
         if ($user->role !== 'blood_bank_admin') {
->>>>>>> fb6e7b4d91676f400e0d4390e9af14f83f4ef560
             abort(403, 'Unauthorized');
         }
 
@@ -35,11 +27,6 @@ class DonorFileController extends Controller
             abort(404, 'File not found.');
         }
 
-<<<<<<< HEAD
-        return response()->file(storage_path("app/" . $path));
-    }
-}
-=======
         $file = Storage::disk('local')->get($path);
 
         $mimeType = Storage::disk('local')->mimeType($path);
@@ -47,4 +34,3 @@ class DonorFileController extends Controller
         return response($file, 200)->header('Content-Type', $mimeType);
     }
 }
->>>>>>> fb6e7b4d91676f400e0d4390e9af14f83f4ef560
